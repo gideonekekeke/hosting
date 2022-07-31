17ca7e3ea2ddf200b9470d20a52e442d3c7ceaf5
gideonekekeke <gideonekeke64@gmail.com>
2022-07-31 22:39:54 +0100

    deployede

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomeScreen from "./Components/HomeScreen";
+import AboutPage from "./Components/AboutPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path='/' element={<HomeScreen />} />
+					<Route path='/about' element={<AboutPage />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
