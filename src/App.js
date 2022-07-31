f396b73355c6c65b02290a1c39c4bf876b22a3f2
gideonekekeke <gideonekeke64@gmail.com>
2022-07-31 23:22:15 +0100

    adding new pagesss

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	HashRouter,
+} from "react-router-dom";
 import HomeScreen from "./Components/HomeScreen";
 import AboutPage from "./Components/AboutPage";
 
 function App() {
 	return (
 		<div>
-			<Router>
+			<HashRouter>
 				<Routes>
 					<Route path='/hosting' element={<HomeScreen />} />
 					<Route path='/about' element={<AboutPage />} />
 				</Routes>
-			</Router>
+			</HashRouter>
 		</div>
 	);
 }
